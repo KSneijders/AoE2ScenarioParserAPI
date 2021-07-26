14f2694a57e5884a8557e63831a21dc68b89ad6a
KSneijders <k.sneijders@st.hanze.nl>
2021-07-26 18:15:04 +0200

    Updated json conversion

--- AoE2ScenarioParserAPI/other/json_converter.py
+++ AoE2ScenarioParserAPI/other/json_converter.py
@@ -1,3 +1,4 @@
+from AoE2ScenarioParser.objects.data_objects.condition import Condition
 from AoE2ScenarioParser.objects.data_objects.effect import Effect
 from AoE2ScenarioParser.objects.data_objects.trigger import Trigger
 
@@ -29,8 +30,8 @@ def trigger_to_dict(trigger: Trigger):
         'trigger_id': 'id'
     }
 
-    effects = list(map(lambda e: effect_to_dict(e), trigger.effects))
-    conditions = list(map(lambda c: condition_to_dict(c), trigger.conditions))
+    effects = [effect_to_dict(e) for e in trigger.effects]
+    conditions = [condition_to_dict(c) for c in trigger.conditions]
 
     return {
         **transform(trigger, exact),
@@ -47,12 +48,12 @@ def effect_to_dict(effect: Effect):
     exact = [
         "effect_type", "ai_script_goal", "armour_attack_quantity", "armour_attack_class", "quantity", "tribute_list",
         "diplomacy", "object_list_unit_id", "source_player", "target_player", "technology", "string_id", "display_time",
-        "trigger_id", "location_x", "location_y", "location_object_reference", "area_1_x", "area_1_y", "area_2_x",
-        "area_2_y", "object_group", "object_type", "instruction_panel_position", "attack_stance", "time_unit",
+        "trigger_id", "location_x", "location_y", "location_object_reference", "area_x1", "area_y1", "area_x2",
+        "area_y2", "object_group", "object_type", "instruction_panel_position", "attack_stance", "time_unit",
         "enabled", "food", "wood", "stone", "gold", "item_id", "flash_object", "force_research_technology",
         "visibility_state", "scroll", "operation", "object_list_unit_id_2", "button_location", "ai_signal_value",
-        "object_attributes", "variable", "timer", "facet", "play_sound", "player_color", "message", "sound_name",
-        "selected_object_ids"
+        "object_attributes", "variable", "timer", "facet", "play_sound", "player_color", "color_mood", "message",
+        "sound_name", "selected_object_ids"
     ]
     to_bool = []
     convert = {}
@@ -64,17 +65,17 @@ def effect_to_dict(effect: Effect):
     }
 
 
-def condition_to_dict(effect: Effect):
+def condition_to_dict(condition: Condition):
     exact = [
         "condition_type", "quantity", "attribute", "unit_object", "next_object", "object_list", "source_player",
-        "technology", "timer", "area_1_x", "area_1_y", "area_2_x", "area_2_y", "object_group", "object_type",
-        "ai_signal", "inverted", "variable", "comparison", "target_player", "unit_ai_action", "xs_function"
+        "technology", "timer", "area_x1", "area_y1", "area_x2", "area_y2", "object_group", "object_type", "ai_signal",
+        "inverted", "variable", "comparison", "target_player", "unit_ai_action", "xs_function",
     ]
     to_bool = []
     convert = {}
 
     return {
-        **transform(effect, exact),
-        **transform(effect, to_bool, 'bool'),
-        **transform(effect, convert),
+        **transform(condition, exact),
+        **transform(condition, to_bool, 'bool'),
+        **transform(condition, convert),
     }
